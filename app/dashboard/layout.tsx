066d0f64fede89d252879e0ec63b0460338d204d
hainmwt <hainmwintel@gmail.com>
2025-01-06 09:31:37 +0700

    update props dashboard/layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,6 @@
 import SideNav from "../ui/dashboard/sidenav";
 
-type Props = {
-  childrens: React.ReactNode;
-};
-
-const Layout = ({ childrens }: Props) => {
+const Layout = ({ childrens }: { childrens: React.ReactNode }) => {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
